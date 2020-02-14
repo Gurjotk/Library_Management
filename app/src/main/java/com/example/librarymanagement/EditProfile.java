de007ebc8966810878e9b948c772f29b8f0acdc2
Admin <ashugill6001@gmail.com>
2020-02-14 11:43:58 -0500

    Edit Profile

--- app/src/main/java/com/example/librarymanagement/EditProfile.java
+++ app/src/main/java/com/example/librarymanagement/EditProfile.java
@@ -2,14 +2,27 @@ package com.example.librarymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditProfile extends AppCompatActivity {
+Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        btn=findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(EditProfile.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
