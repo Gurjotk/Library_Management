2544ae950df9626876c3a7f21bb712b4f6b4c413
Gurjot <gurjotkaur06255@gmail.com>
2020-02-14 14:55:11 -0500

    start from login page

--- app/src/main/java/com/example/librarymanagement/Login.java
+++ app/src/main/java/com/example/librarymanagement/Login.java
@@ -1,5 +1,27 @@
 package com.example.librarymanagement;
 
-public class Login {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    Button l;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+l=findViewById(R.id.login);
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 }
