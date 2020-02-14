e63c04a17ff88225a5b2f40858fb4c203aca8ecb
Gurjot <gurjotkaur06255@gmail.com>
2020-02-14 14:16:19 -0500

    contact

--- app/src/main/java/com/example/librarymanagement/MainActivity.java
+++ app/src/main/java/com/example/librarymanagement/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(MainActivity.this,contact.class);
+                Intent i= new Intent(MainActivity.this,contactus.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/librarymanagement/contactus.java
+++ app/src/main/java/com/example/librarymanagement/contactus.java
@@ -1,22 +1,23 @@
 package com.example.librarymanagement;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.os.Bundle;
 import android.widget.Button;
 
-public class contact extends AppCompatActivity {
-    Button c;
+public class contactus extends AppCompatActivity {
+Button c;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contact);
+        setContentView(R.layout.activity_contactus);
         c= findViewById(R.id.buttonc);
-       c.setOnClickListener(new View.OnClickListener() {
+        c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(contact.this,MainActivity.class);
+                Intent i= new Intent(contactus.this,MainActivity.class);
                 startActivity(i);
             }
         });
