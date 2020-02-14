c3162ce3bbed9083452ac37410236125b54ac7f0
Admin <ashugill6001@gmail.com>
2020-02-14 12:27:02 -0500

    categories

--- app/src/main/java/com/example/librarymanagement/MainActivity.java
+++ app/src/main/java/com/example/librarymanagement/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button b;
+    Button edp;
 
 
     @Override
@@ -16,12 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        b= findViewById(R.id.contact);
+       edp=findViewById(R.id.profile);
+       edp.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent i= new Intent(MainActivity.this,EditProfile.class);
+               startActivity(i);
+           }
+       });
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i= new Intent(MainActivity.this,contact.class);
                 startActivity(i);
+
             }
         });
+
     }
 }
